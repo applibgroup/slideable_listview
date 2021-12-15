cef547d021ad1c501e987bcc15aea35ac023a1cd
Ratnakar <ratnakar3895@yahoo.com>
2021-12-15 16:29:02 +0530

    feat: initial view added

---
+++ entry/src/main/java/com/example/testapp/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.testapp;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/ohosTest/java/com/example/testapp/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.example.testapp;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.example.testapp.hmservice", actualBundleName);
+    }
+}
