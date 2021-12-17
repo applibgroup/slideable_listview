5c0947407e43654a5e68cc5ed96334d7eeaa6225
Ratnakar <ratnakar3895@yahoo.com>
2021-12-17 10:39:47 +0530

    feat: swiping feature added

---
+++ test_lib/lib/components/comp.js
@@ -0,0 +1,10 @@
+export default {
+    data: {
+        index: 1,
+        display: true,
+    },
+    swipeLeft: function (element) {
+        // this.display = false;
+        element.style.display = "none";
+    },
+};
